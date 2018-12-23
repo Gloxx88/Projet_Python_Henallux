2b52af8918d0c5542de4d6851258fc52eb481ec0
WhataDude <46083446+WhataDude@users.noreply.github.com>
2018-12-23 10:49:10 +0100

    Add files via upload

---
+++ coucou.py
@@ -0,0 +1,15 @@
+from Objet_Us_Target import Target
+
+
+target = Target()
+target.socket_bind()
+target.socket_accept()
+try:
+    target.what_to_do()
+except ConnectionResetError as msg:
+    if target.print:
+        print("The connection has been stopped")
+        print("Error : " + str(msg))
+    target.quit()
+
+#je fais un test
